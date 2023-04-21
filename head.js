9d798baf5d79300f35aedcd02e1b03a3f3a4317d
MysticsGitHub <bradyjames97@hotmail.com>
2023-04-21 18:12:48 +0000

    fix error with eslint

--- head.js
+++ head.js
@@ -8,8 +8,8 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], undefined))
+assertEqual(head([], undefined));
