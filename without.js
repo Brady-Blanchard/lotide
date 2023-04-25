fd72fec5c69545280364fc99fb3706de1efa32d8
MysticsGitHub <bradyjames97@hotmail.com>
2023-04-25 07:20:51 +0000

    Add without.js

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.includes(source[i]) === false) {
+      newArray.push(source[i]);
+    }
+  }
+  return console.log(newArray);
+};
+
+
+//test cases
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
