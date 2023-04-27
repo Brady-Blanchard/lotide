533422bca1fa8944307291a94a21230479787b00
MysticsGitHub <bradyjames97@hotmail.com>
2023-04-27 16:25:22 +0000

    Add comments to explain code

--- eqArrays.js
+++ eqArrays.js
@@ -8,15 +8,19 @@ const assertEqual = function(actual, expected) {
 
 // Implement function that takes 2 arrays and returns true or false, based on a perfect match
 
+// function for testing the equality of arrays
 const eqArrays = function(arr1, arr2) {
+  // if arrays don't have the same length return false
   if (arr1.length !== arr2.length) {
     return false;
   }
+  //if arrays don't have the same values return false
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
+  //if none of those happen return true
   return true;
 };
 
