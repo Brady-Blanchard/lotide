6223760f6a14d1e8b0157feff931ba6b031ef832
MysticsGitHub <bradyjames97@hotmail.com>
2023-04-27 16:34:50 +0000

    Changed the term value to element

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,7 +4,7 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  //if arrays don't have the same values return false
+  //if arrays don't have the same elements return false
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
