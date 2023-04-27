35148b7f285c572218a0b39520f58ee040424ed2
MysticsGitHub <bradyjames97@hotmail.com>
2023-04-27 12:07:26 +0000

    Add countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  //loops over keys in allItems
+  for (const item of allItems) {
+    //prints each key to console
+    console.log(item);
+    //if the key is true in itemsToCount then continue to next if
+    if (itemsToCount[item]) {
+      //if key exists in results increment it by 1 else initialize it to 1
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+// TEST CODE
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouthanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log(result1);
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
