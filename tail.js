1da1ef2ad9a65fee73a5f52b00516b44f9e3ccf4
MysticsGitHub <bradyjames97@hotmail.com>
2023-05-13 21:53:33 +0000

    exported tail func, add tailTest

--- tail.js
+++ tail.js
@@ -1,20 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// returns last element in an array
 const tail = function(array) {
   return array.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// EXPORT FUNCTION
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+// require the assertEqual and tail function
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
