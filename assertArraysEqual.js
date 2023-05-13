32e1a828723e6965056a128424fc66afc583ca35
MysticsGitHub <bradyjames97@hotmail.com>
2023-05-13 22:19:54 +0000

    refacored assertArraysEqual func, add assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,5 @@
-// function for testing the equality of arrays
-const eqArrays = function(arr1, arr2) {
-  // if arrays don't have the same length return false
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  //if arrays don't have the same elements return false
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  //if none of those happen return true
-  return true;
-};
+// require eqArrays function
+const eqArrays = require('./eqArrays')
 
 // function for asserting two arrays equality
 const assertArraysEqual = function(actual, expected) {
@@ -25,8 +12,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+// require eqArrays function
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
