44a84b1dbdf239b152cca7e1ab1a9575022999ca
MysticsGitHub <bradyjames97@hotmail.com>
2023-05-15 22:31:07 +0000

    add mocha & chai, refactored headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,22 @@
-// require the assertEqual and head function
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-
-
+// require the chai assert func and head function
+const assert = require('chai').assert;
+const head   = require('../head');
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([], undefined));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+});
