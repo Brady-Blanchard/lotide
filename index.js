6420495d7ce0b47dd6948d467a29d454485c6be9
Brady-Blanchard <bradyjames97@hotmail.com>
2023-05-22 06:45:45 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
